69a6a17bb234e736e1bdd4d9d21e126543566963
Justin Rusbatch <jrusbatch@gmail.com>
2019-12-06 17:44:17 -0500

    GetRequestUriRelativeToRoot should ignore port numbers in the URI

--- src/OpenRasta/Web/Internal/CommunicationContextExtensions.cs
+++ src/OpenRasta/Web/Internal/CommunicationContextExtensions.cs
@@ -1,4 +1,6 @@
 using System;
+using OpenRasta.Diagnostics;
+using OpenRasta.Pipeline.Diagnostics;
 
 namespace OpenRasta.Web.Internal
 {
@@ -6,9 +8,18 @@ namespace OpenRasta.Web.Internal
     {
         public static Uri GetRequestUriRelativeToRoot(this ICommunicationContext context)
         {
+            var requestUri = context.Request.Uri;
+
+            // Normalize non-standard port numbers based on the URI scheme.
+            var port = requestUri.Scheme == "https" ? 443 : 80;
+            var builder = new UriBuilder(requestUri.Scheme, requestUri.Host, port, requestUri.AbsolutePath);
+            builder.Query = requestUri.Query.TrimStart('?');
+
+            var uri = new Uri(builder.Uri.ToString());
+
             return context.ApplicationBaseUri
                 .EnsureHasTrailingSlash()
-                .MakeRelativeUri(context.Request.Uri)
+                .MakeRelativeUri(uri) // Strip standard port numbers from the URL.
                 .MakeAbsolute("http://localhost");
         }
         
