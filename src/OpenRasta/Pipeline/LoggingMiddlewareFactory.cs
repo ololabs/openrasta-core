7dcf2ec422eac4c74c01b57959aa2dbe924fbf8d
Justin Rusbatch <jrusbatch@gmail.com>
2020-02-13 14:56:26 -0500

    OpenRasta: Make logging middleware optional

--- src/OpenRasta/Pipeline/LoggingMiddlewareFactory.cs
+++ src/OpenRasta/Pipeline/LoggingMiddlewareFactory.cs
@@ -1,10 +1,16 @@
-﻿namespace OpenRasta.Pipeline
+﻿using System.Configuration;
+
+namespace OpenRasta.Pipeline
 {
   public class LoggingMiddlewareFactory : IPipelineMiddlewareFactory
   {
     public IPipelineMiddleware Compose(IPipelineMiddleware next)
     {
-      return new LoggingMiddleware(next);
+      if (!bool.TryParse(ConfigurationManager.AppSettings["openrasta:DisableLoggingMiddleware"], out var setting) || !setting)
+      {
+        return new LoggingMiddleware(next);
+      }
+      return next;
     }
   }
 }
