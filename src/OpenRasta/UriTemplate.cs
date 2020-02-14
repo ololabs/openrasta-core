b8b528607d4d8dec88f599bbdef8d3c25c7f5c3e
Jack Mott <jack.mott@gmail.com>
2020-02-14 14:53:45 -0600

    OR: remove unused using [PLAT-23274]

--- src/OpenRasta/UriTemplate.cs
+++ src/OpenRasta/UriTemplate.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace OpenRasta
